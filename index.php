2e297298a60261bc92b0826aa61ee6d85feef6a1
Samuel Allan Valerie <17672@csmrouen.net>
2019-02-12 16:20:15 +0100

    Ajout extension inc au php

--- index.php
+++ index.php
@@ -2,6 +2,19 @@
 date_default_timezone_set ('Europe/Paris');
 include "./includes/header.php" ;
 
+if (isset($_GET['page'])) {
+    $page = $_GET['page'];
+}
+
+else {
+    $page = "accueil";
+}
+
+$page = isset($_GET['page']) ? $_GET['page'] : "accueil";
+
+$page = "./includes/" . $page . ".php";
+include $page;
+
+include "./includes/index.php";
 
 
-include "./includes/footer.php" ;
