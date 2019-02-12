edcb95cb17425d0169fa193609bdc1301290b252
Samuel Allan Valerie <17672@csmrouen.net>
2019-02-12 14:38:29 +0100

    Menu header with style.

--- index.php
+++ index.php
@@ -1,16 +1,2 @@
-<ul>
-    <li><a href="index.php?page=truc">truc</a></li>
-    <li><a href="index.php?page=machin">machin</a></li>
-    <li><a href="index.php?page=michel">michel</a></li>
-</ul>
 <?php
-
-if (isset ($_GET['page'])) {
-    $page = $_GET['page'];
-}
-
-else {
-    $page = "message";
-}
-
-echo $page;
+include "./includes/header.php" ;
