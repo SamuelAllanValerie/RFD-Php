8b6a372da4624d6d8d5090810f236aabf1c7d056
Samuel Allan Valerie <17672@csmrouen.net>
2019-02-13 16:57:53 +0100

    ajout version 4.7

--- includes/footer.php
+++ includes/footer.php
@@ -3,7 +3,7 @@ $date = date('Y');
 ?>
 </main>
 <footer>
-    <p> <?=$date?> - &copy; NFactory Corp.</p>
+    <p> &copy;<?=$date?> -  NFactory Corp.</p>
 </footer>
     <script src=".assets/javascript/jquery.js" type="text/javascript"</script>
     <script src=".assets/javascript/bootstrap.js" type="text/javascript"</script>
