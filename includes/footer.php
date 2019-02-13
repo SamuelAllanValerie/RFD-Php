0a882317988257631a1119b3bf7eed82f26c3f60
Samuel Allan Valerie <17672@csmrouen.net>
2019-02-13 15:12:49 +0100

    ajout version 4.2 with no errors

--- includes/footer.php
+++ includes/footer.php
@@ -3,7 +3,7 @@ $date = date('Y');
 ?>
 </main>
 <footer>
-    <p> <?=$date?> - NFactory Corp.</p>
+    <p> <?=$date?> - &copy; NFactory Corp.</p>
 </footer>
     <script src=".assets/javascript/jquery.js" type="text/javascript"</script>
     <script src=".assets/javascript/bootstrap.js" type="text/javascript"</script>
--- includes/inscription.inc.php
+++ includes/inscription.inc.php
@@ -6,9 +6,22 @@ if (isset($_POST['maurice'])) {
     $mail = isset($_POST['mail']) ? $_POST['mail'] : "";
     $mdp = isset($_POST['mdp']) ? $_POST['mdp'] : "";
 
+    $erreurs = array();
 
-    }
+    if (!(mb_strlen($nom) >= 2 && ctype_alpha($nom)))
+        array_push($erreurs,"Veuillez saisir un nom correct.");
 
-    else {
+    if (!filter_var($mail, FILTER_VALIDATE_EMAIL))
+        array_push($erreurs,"Veuillez saisir une adresse mail valide.");
+
+    if (mb_strlen($mdp) <6)
+        array_push( $erreurs,"Votre mot de passe doit comporter 6 caracteres minimum");
+
+    echo "<pre>";
+    print_r($erreurs);
+    echo "</pre>";
+}
+
+else {
         require_once "frminscription.php";
 }
